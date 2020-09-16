6c716ef8614e22e7af7fd937e079b9c979a8aae8
kayvera <mikaylarvr@gmail.com>
2020-09-16 15:09:37 -0700

    added backend that connects to mongodb, authenticates user information

--- backend/index.js
+++ backend/index.js
@@ -13,4 +13,21 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
-//set up mongoose
+// set up mongoose
+
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
+
+//setup routes
+
+app.use("/users", require("./routes/userRouter"));
