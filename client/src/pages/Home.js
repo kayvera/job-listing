7f932238e91c245d8e2e540200faacc35e72c7b7
kayvera <mikaylarvr@gmail.com>
2020-09-23 11:48:09 -0700

    .

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import UserContext from "../context/UserContext";
+import JobFilter from "../components/jobboard/JobFilter";
+
+const Home = () => {
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!userData.user) history.push("/login");
+  });
+  return (
+    <div>
+      <JobFilter />
+    </div>
+  );
+};
+
+export default Home;
