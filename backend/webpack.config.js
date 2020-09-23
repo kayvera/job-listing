1cdf43c1e02c8fb23c0062e3a3ececffd1d67b3f
kayvera <mikaylarvr@gmail.com>
2020-09-23 11:54:52 -0700

    added webpack

---
+++ backend/webpack.config.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+};
